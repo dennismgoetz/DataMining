2b95df60602e0cbe2340d9be989d5d1f553562e6
Bromsnortor <f.w.j.vanderwaals@student.utwente.nl>
2024-01-08 21:18:42 +0100

    added comments to my parts of the code

--- Solutions/routemining.py
+++ Solutions/routemining.py
@@ -1,22 +1,26 @@
 # %%
 def findroutes(filename, limit_data=0, driver_id=0, prints=False):    
+    #this function finds the best routes from all the input data. The amount of routes it presents is the same as the amount of standard 
+    #routes that are in the input file. It clusters the trips and then finds most frequent maximal sequences
+    
     import json
     import math
-    import numpy as np
     import warnings
-    import matplotlib.pyplot as plt
     import random
     from sklearn.cluster import KMeans
     from sklearn.metrics import silhouette_score
     from sklearn.preprocessing import MinMaxScaler
 
-    def load_json(filename):
-        with open(filename, 'r') as file:
-            data4 = json.load(file)
-        return data4
 
     def load_data(actual_routes_file1, limit_actual_routes1=0, driver_id=0):
-        #load data and limit it as required
+        #load data from file and limit it as required
+        
+        def load_json(filename):
+            #loads the json file into a variable
+            with open(filename, 'r') as file:
+                data4 = json.load(file)
+            return data4
+
         
         #load driverdata
         driver_data1=load_json(actual_routes_file1)
@@ -65,7 +69,7 @@ def findroutes(filename, limit_data=0, driver_id=0, prints=False):
         return dim_count, len(standardroutes)
 
     def createpoints(data, dim_count):   
-        #convert the data into lists of data points so the clustering can be applied
+        #convert the data into a dictionary with for each connection some lists of data points so the clustering can be applied
         data_points={}
         for conn_name in dim_count:
             data_points[conn_name]=[]
@@ -81,35 +85,41 @@ def findroutes(filename, limit_data=0, driver_id=0, prints=False):
                     temp_point[index]=trip_info["merchandise"][merch]
                             
                 data_points[conn_name].append(temp_point)
-        
+                
         return data_points
 
     def clustering(data_points):
-        #find all clusters
+        #find all clusters by first determining the amount of clusters with sampling and then applying kmeans witht the found amount of clusters
+        #output is a dictionary with for every city a dictionary for every data point in that city with a label to it for the specific cluster.
 
         #ignore warnings temporarily for better readability
         warnings.filterwarnings("ignore")
 
         def sample(upper_limit1, amount_of_samples1, data1):
+            #function to only take some points of the data.
             rand_numbs=random.sample(range(0, upper_limit1), amount_of_samples1)
             sample_space=[list(data1[i]) for i in rand_numbs]
             return sample_space
 
         def round_list(lst, decimal_places=0):
+            #function to rount all the elements in a list or matrix
             rounded_list = [round(element, decimal_places) for element in lst]
             return rounded_list
 
+        #for every citypair we need to find the clusters 
         ext_data_points={}
         clusterinfo={}
-        count=0
+    
         for city in data_points:
-            count=count+1
+            #first find the amount of samples to be taken
+            #this amount grows with the size of the datapoints for that specific citypair
+            #also find the amount of clusters maximally are expecting to find.
             max_expected_clusters=0
             labeling={}
             data = data_points[city]
-            #limit the sample space
             datasize=len(data)
             upper_limit=datasize
+            
             if datasize<1000:
                 amount_of_samples=min(datasize, 100)
                 max_expected_clusters=round(float(amount_of_samples)**(1/2))
@@ -125,7 +135,8 @@ def findroutes(filename, limit_data=0, driver_id=0, prints=False):
             
             #check if all the samplepoint    print(amount_of_samples)
             #are the same and if they are try again 5 times
-            #if still the same pass the knowledge on
+            #if still the same pass the knowledge
+            #we do this because silhouette score for one datapoint doesn't exist and we don't need to cluster when all points are the same.
             for _ in range(5):
                 one_point_marker=True
                 sample_space=sample(upper_limit, amount_of_samples, data)
@@ -133,16 +144,17 @@ def findroutes(filename, limit_data=0, driver_id=0, prints=False):
                     one_point_marker=False
                     break
                     
-            # Calculate silhouette scores for different values of k using samples 
-            silhouette_scores = []
-            #limit expected amount of clusters proportional to the amount of datapoints we can maybe say that for every 2% of dataset there may be a cluster existing
+            # Calculate silhouette scores for different values of k using samples:
             
+            silhouette_scores = []
+            #limit expected amount of clusters proportional to the amount of datapoints 
             K_range = range(2, max_expected_clusters)
 
-
-            
+            #if two or more different points are present in the subspace of the citypair and the max_expected_cluster is more than 2 
+            #(more than two because if it is two this means dataset is very, very small)
             if one_point_marker==False and max_expected_clusters>2:
-                #loop through possible amount of clusters and stop for first silhouette with score >0.7
+                #loop through possible amount of clusters and stop for first silhouette with score >0.7, this saves time
+                #and we are not looking for big number of clusters (this is done with minmaxnormalisation)
                 for k in K_range:
                     try:
                         scaler = MinMaxScaler()
@@ -156,16 +168,15 @@ def findroutes(filename, limit_data=0, driver_id=0, prints=False):
                     if max(silhouette_scores)>0.7:
                         break
                 
-                #determine k using the sampled space
+                #determine k using the sampled space, if silhouette score is not big enough 1 clusters is expected.
                 max_ss=max(silhouette_scores)
                 if max_ss>0.6:
-                    k=silhouette_scores.index(max_ss)+2
+                    k=silhouette_scores.index(max_ss)+2 #plus 2 is added because of code practicalities
                 else:
                     k=1
-                    
             else:
                 k=1
-            #now use kmeans to find the labels of the points for the entire set
+            #now use kmeans to find the labels of the points for the entire set (with normalised data)
             scaler = MinMaxScaler()
             data = scaler.fit_transform(data)
             kmeans = KMeans(n_clusters=k)
@@ -174,11 +185,12 @@ def findroutes(filename, limit_data=0, driver_id=0, prints=False):
             #build the extended data points set which also contain the labels of the points
             labels=kmeans.labels_
             
+            #build the dictionary with for every city a dictionary for every data point in that city with a label to it for the specific cluster by using the gathered data
             for index, point in enumerate(data_points[city]):
                 labeling[tuple(point)]=labels[index]
             ext_data_points[city]=labeling
             
-            #build a dataset which contains info about each cluster
+            #build a dataset which contains info about each cluster (count, inertia and centroid)
             for i in range(k):
                 temp={}
                 oneclusterdata=data[labels==i]
@@ -223,7 +235,7 @@ def findroutes(filename, limit_data=0, driver_id=0, prints=False):
         return tuple(dataset)
 
     def find_freq_routes(dataset,dim_map ,NS):
-
+        
         def FREQUENT_ITEMS(dataset,thresehold):
             def subsequence(m,S):
                 #this function tells us wether a given sequence is a subsequence of a longer one.
@@ -472,33 +484,45 @@ def findroutes(filename, limit_data=0, driver_id=0, prints=False):
 
 # %%
 def rankroutes(actual_routes_file, routes_to_sort_file, driver_id, limit_actual_routes=0, limit_routes_to_sort=0, prints=False, numperm3=128, findbest=0):
+    #this function takes all the data of one specific driver and routes to be sorted and sorts these routes based on similarity between the routes that the 
+    #driver has driven and the  routes to be sorted. It does this by converting the routes into sets with keywords and then creating signatures for every route
+    #with minhashing and then comparing every route to be sorted with every driven route and taking the average.
+    
     from datasketch import MinHash
     import json
     
-    def convert_frozensets_to_string(input_set):
-        result_set = set()
-        for element in input_set:
-            if isinstance(element, frozenset):
-                for i, merch in enumerate(element):
-                    if i==0:
-                        result_string=merch
-                    else:
-                        result_string += "-"+str(merch)
-                result_set.add(result_string)
-            else:
-                result_set.add(element)
-        return result_set
 
-    def create_minhash(set_data, numperm4):
-        minhash = MinHash(num_perm=numperm4)
-        for item in set_data:
-            minhash.update(item.encode('utf-8'))
-        return minhash
-
-    def calculate_jaccard_similarity(minhash1, minhash2):
-        return minhash1.jaccard(minhash2)
 
     def route_to_minhash(data1, mapborders, num_perm5):
+        #this function creates a set out with keywords out of the input route
+        #this set of keywords is converted into a signature using minhashing
+        
+        def create_minhash(set_data, numperm4):
+        #create minhash for the input set
+            minhash = MinHash(num_perm=numperm4)
+            for item in set_data:
+                minhash.update(item.encode('utf-8'))
+            return minhash
+        
+        def convert_frozensets_to_string(input_set):
+            #practicality function:
+            #to keep the order of the sets it was frozen but it needs to be converted to an element for the set instead of a frozenzet
+            #this function does that
+            result_set = set()
+            for element in input_set:
+                if isinstance(element, frozenset):
+                    for i, merch in enumerate(element):
+                        if i==0:
+                            result_string=merch
+                        else:
+                            result_string += "-"+str(merch)
+                    result_set.add(result_string)
+                else:
+                    result_set.add(element)
+            return result_set
+
+        
+        #the following elements are added to the set of every route:
         #0add the single cities
         #1add the conn between cities
         #2add single merch
@@ -507,7 +531,6 @@ def rankroutes(actual_routes_file, routes_to_sort_file, driver_id, limit_actual_
         #5combine "from" city with product and "to" city with product (without adding from or to)
         #6combine products with every other product within a specific trip 
 
-        #add all the generated sets to a minhashing list
         minhash_list=[]
         
         for route in data1:
@@ -538,6 +561,7 @@ def rankroutes(actual_routes_file, routes_to_sort_file, driver_id, limit_actual_
                     for comb in trip["merchandise"]:
                         if comb != merch:
                             vector.add(frozenset((comb, merch)))
+                            
             #first normalise the vector to set elements or tuples
             vector=convert_frozensets_to_string(vector)
             #then add the set to the minhash_list with the reference actual route
@@ -549,7 +573,7 @@ def rankroutes(actual_routes_file, routes_to_sort_file, driver_id, limit_actual_
         #the amount is specified by a indicator (small, medium, large)
         #to do that the max and min is found for every merchandise
 
-        #find min and max for a merch and the possible merch to be carried 
+        #find min and max for a mdimensionserch and the possible merch to be carried 
         mapminmax={}
         possiblecomb={}
 
@@ -584,6 +608,12 @@ def rankroutes(actual_routes_file, routes_to_sort_file, driver_id, limit_actual_
         return mapborders
 
     def rank_routes(minhaslistdata, minhashlistroutes):
+        #take all signatures of actual routes and compare them with route to be sorted one at a time and find average similarity 
+        
+        def calculate_jaccard_similarity(minhash1, minhash2):
+            #compute jaccard similarity between two minhashes 
+            return minhash1.jaccard(minhash2)
+
         score1=[]
         for routes1 in minhashlistroutes:
             summation=0.0
@@ -592,7 +622,7 @@ def rankroutes(actual_routes_file, routes_to_sort_file, driver_id, limit_actual_
             score1.append([routes1[0], summation/len(minhaslistdata)])
         score1=sorted(score1, key=lambda x: x[1], reverse=True)
         
-        #now only output the routes without rating:
+        #now only output the routes without the rating added to it (we only need the order not the score):
         routes=[]
         for element in score1:
             routes.append(element[0])
@@ -600,6 +630,7 @@ def rankroutes(actual_routes_file, routes_to_sort_file, driver_id, limit_actual_
         return routes
 
     def load_json(filename):
+        #load json file into a variable
         with open(filename, 'r') as file:
             data4 = json.load(file)
         return data4
@@ -618,7 +649,7 @@ def rankroutes(actual_routes_file, routes_to_sort_file, driver_id, limit_actual_
             if route["driver"]==driver_id:
                 driver_data1.append(route)
 
-        #if a limit on total routes is set apply it
+        #if a limit on total routes is set, apply it
         if limit_actual_routes1!=0:
             driver_data1=driver_data1[:limit_actual_routes1]
         if limit_routes_to_sort1!=0:
@@ -645,6 +676,7 @@ def rankroutes(actual_routes_file, routes_to_sort_file, driver_id, limit_actual_
             driver_data1=driver_data1[:limit_actual_routes1]
         return driver_data1
 
+    #this function has a small feature where you can say that the routes to be ranked can be given as a variable instead of a filelocation (for function3)
     if(prints==True):  print("loading data")
     if findbest!=0:
         driver_data3=load_data2(actual_routes_file, driver_id, limit_actual_routes)
@@ -659,9 +691,10 @@ def rankroutes(actual_routes_file, routes_to_sort_file, driver_id, limit_actual_
     minhash_list_data1=route_to_minhash(driver_data3, mapborders, numperm3)
     minhash_list_rankroutes1=route_to_minhash(rank_routes3, mapborders, numperm3)
         
-    if(prints==True): print("comparing using jaccard")
+    if(prints==True): print("comparing using jaccard similarity")
     result=rank_routes(minhash_list_data1, minhash_list_rankroutes1)
 
+    #if this function is used for solution3 it outputs entire routes instead of the id's
     if findbest!=0:
         for routes in routes_to_sort_file:
             if routes["id"]==result[0]:
@@ -671,6 +704,8 @@ def rankroutes(actual_routes_file, routes_to_sort_file, driver_id, limit_actual_
 
 # %%
 def findbestroute(actual_routes_file, driver_id, limit_data=0, prints=False,  printsalot=False, numperm=128):
+    #this function uses the data of one specific driver to: first find some routes that are good for the driver.
+    #then sort these routes in case they weren't good enough 
     
     if (prints==True): print("Generating routes")
     routes=findroutes(actual_routes_file, limit_data, driver_id, prints=printsalot)
